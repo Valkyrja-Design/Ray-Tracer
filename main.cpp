a4d8361c36409a84a363bba4df96d2bf4ddd511d
Manish <200050071@iitb.ac.in>
2022-06-01 23:55:37 +0530

    Add sphere and viewport

---
+++ main.cpp
@@ -0,0 +1,58 @@
+#include "utilities/color.h"
+#include "utilities/vec3.h"
+#include "utilities/ray.h"
+
+#include <iostream>
+
+using namespace std;
+
+bool hit_sphere(const point3& center, double radius, const ray& r){
+    auto oc = r.origin() - center;
+    auto b = 2*(dot(r.direction(), oc));
+    auto a = r.direction().length_squared();
+    auto c = oc.length_squared()-radius*radius;
+    return (b*b-4*a*c >= 0);
+}
+
+color ray_color(const ray& r){
+    if (hit_sphere(point3(0, 0, -1), 0.5, r)){
+        return color(1, 0, 0);
+    }
+    vec3 unit_direction = unit_vector(r.direction());
+    auto t = 0.5*(unit_direction.y()+1.0);
+    // Blends color between white and blue according to the y height of ray direction
+    return (1.0-t)*color(1.0, 1.0, 1.0)+t*color(0.5, 0.7, 1.0);
+}   
+
+int main(){
+    // Image dimensions
+    const auto aspect_ratio = 16.0/9.0;
+    const int image_width = 400;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+    // Camera 
+
+    auto viewport_height = 2.0;
+    auto viewport_width = aspect_ratio*viewport_height;
+    auto focal_length = 1.0;
+
+    auto origin = point3(0, 0, 0);
+    auto horizontal = point3(viewport_width, 0, 0);
+    auto vertical = point3(0, viewport_height, 0);
+    auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
+
+    //Render 
+    
+    cout<<"P3\n"<<image_width<<" "<<image_height<<"\n255\n";
+    for (int j = image_height-1; j>=0; j--){
+        cerr<<"\rLines remaining: "<<j<<" "<<flush;
+        for (int i=0;i<image_width;i++){
+            double u = double(i) / (image_width - 1);
+            double v = double(j) / (image_height - 1);
+            ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
+            color pixel_color = ray_color(r);
+            write_color(cout, pixel_color);
+        }        
+    }
+    cerr<<"\nDone.\n";
+}
